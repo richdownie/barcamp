bfff336f69baae8b15af860f6c2e17e28b656897
Rich Downie <rich.downie@newbrandanalytics.com>
2014-04-16 20:23:35 -0400

    updates

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,12 +2,26 @@ require 'blackboxd'
 require 'cucumber'
 require 'selenium-webdriver'
 require 'rspec'
+require 'gmail'
   
 Before do
   @driver = Selenium::WebDriver.for :chrome	
   @time = 15
 end
 
-After do 
+After do |scenario|
+  if(scenario.failed?)
+    gmail = Gmail.connect("<email>@gmail.com", "<password>")
+    gmail.deliver do
+      to "<email>"
+      subject "Message"
+      text_part do
+        body "#{scenario.backtrace_line}"
+      end
+      html_part do
+        body "<p>#{scenario.backtrace_line}</p>"
+      end
+    end
+  end
   @driver.quit
 end
